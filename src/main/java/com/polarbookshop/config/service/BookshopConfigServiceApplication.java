da934c4dd027f0cdd495cf0bc2dee10c41364704
RomanHreits <romagreic@gmail.com>
2025-06-01 13:15:41 +0200

    first commit

---
+++ src/main/java/com/polarbookshop/config/service/BookshopConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.config.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopConfigServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/polarbookshop/config/service/BookshopConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.config.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
